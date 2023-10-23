67192c9b5cd7dcfc0630676baa10de1d899607d8
SrUsuario <115171836+SrUsuario@users.noreply.github.com>
2023-10-23 20:28:23 +0200

    Añadido delay en el js y mejorado el modo oscuro

--- js/modeDark.js
+++ js/modeDark.js
@@ -1,17 +1,26 @@
 // Constantes para los elementos con los ID (ahorra código al no repetir lo mismo varias veces)
 const SWITCH = document.getElementById("switch");
 const INFO_SWITCH = document.getElementById("infoSwitch");
+const DELAY = 2000;
 
 // Listener que escucha cuando hay click en el checkbox (el switch)
-SWITCH.addEventListener("click", modeDark, false);
+SWITCH.addEventListener("click", modeDark);
 
 // Cambia entre tema claro y tema oscuro
 function modeDark() {
   if (SWITCH.checked) {
-    document.body.classList.add("dark");
-    INFO_SWITCH.innerHTML = "Esta página tiene el tema oscuro";
+    SWITCH.disabled = true;
+    setTimeout(() => {
+      document.body.classList.add("dark");
+      INFO_SWITCH.innerHTML = "Esta página tiene el tema oscuro";
+      SWITCH.disabled = false;
+    }, DELAY);
   } else {
-    document.body.classList.remove("dark");
-    INFO_SWITCH.innerHTML = "Esta página tiene el tema claro";
+    SWITCH.disabled = true;
+    setTimeout(() => {
+      document.body.classList.remove("dark");
+      INFO_SWITCH.innerHTML = "Esta página tiene el tema claro";
+      SWITCH.disabled = false;
+    }, DELAY);
   }
 }
